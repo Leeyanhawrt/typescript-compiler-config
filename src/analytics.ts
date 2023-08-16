fc5609f6540948bb2ba0b8dd07a9056d4370da82
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-15 21:15:56 -0400

    Finish going over strict options

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,1 +1,9 @@
-console.log("Sending...");
+let logged;
+
+// If data is not properly typed it will not compile with noImplicitAny (All params must be declared)
+const sendAnalytics = (data: string) => {
+  console.log(data);
+  logged = true; // It's okay because a certain type is not expected and can be tracked by TS
+};
+
+sendAnalytics("The data");
