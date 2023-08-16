ee011c125a092d9a2277f1638d48fceca8fe10d8
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-15 21:52:28 -0400

    Go over additional configs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 const button = document.querySelector(".button");
 
 if (button) {
-  // StrictNullChecks helps with possible null values (e.g this button does not appear). In order to combat it, use ! at the end of selector OR wrap in if statement so that it is also checked during runtime
+  // StrictNullChecks helps with possible null values (e.g this button does not appear). In order to combat it, use ! at the end of selector OR wrap in if statement so that it is also checked during runtime.
   button.addEventListener("click", () => {
     console.log("Clicked on button");
   });
